da5924e7e001a6cc69a887d78355cd113ea331fe
nishant <nishant.singh87@gmail.com>
2017-02-26 13:37:29 +0530

    add firebase.

--- index.js
+++ index.js
@@ -3,9 +3,12 @@ var express 		= require('express'),
     logger 			= require('logops'),
 		fs 					= require('fs'),
 		bodyParser 	= require('body-parser'),
+		firebase    = require('firebase'),
 
 		CONFIG_PATH     = './config',
-		CONSTANTS_PATH  = 'CONFIG_PATH/constants.json'.replace('CONFIG_PATH', CONFIG_PATH);
+		CONFIG_FILE  		= 'CONFIG_PATH/config.json'.replace('CONFIG_PATH', CONFIG_PATH),
+		CONSTANTS_PATH  = 'CONFIG_PATH/constants.json'.replace('CONFIG_PATH', CONFIG_PATH),
+		CONFIG    		  = JSON.parse(fs.readFileSync(CONFIG_FILE, 'utf8')),
 		CONSTANTS    		= JSON.parse(fs.readFileSync(CONSTANTS_PATH, 'utf8'));
 
 app.use(expressLogging(logger));
@@ -23,3 +26,24 @@ app.use(function(req, res, next) {
 
 require("./src/app").launch(app);
 require("./src/routes").load(app);
+
+var config = {
+			apiKey						: process.env.CLIENT_KEY,
+			authDomain				: CONFIG.firebase.authDomain,
+			databaseURL				: CONFIG.firebase.databaseURL,
+			storageBucket		 	: CONFIG.firebase.storageBucket,
+			messagingSenderId	: CONFIG.firebase.messagingSenderId,
+		},
+		database = firebase.initializeApp(config).database().ref("client-storage"),
+		usersDb = database.child("users");
+
+usersDb.on("value", function(users){
+	console.log("Changed : ");
+	//console.log(JSON.stringify(users));
+});
+
+usersDb.push({
+	"name": "User One",
+	"Id":   "User Two",
+});
+
