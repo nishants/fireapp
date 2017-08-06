32075c6d7f1b9438becf7d718aa881b57406903f
nishant <nishant.singh87@gmail.com>
2017-08-06 15:04:02 +0530

    Add ids to reports.

--- src/crg/reports.js
+++ src/crg/reports.js
@@ -1,11 +1,14 @@
 var Promise = require("promise"),
     Client = require("../client"),
     notNull = function(passage){return !!passage;},
+    withId   = function(passage, index){
+      passage && (passage.id = index);
+      return passage;},
     Reports = {
       allPassages: function(){
         return new Promise(function (resolve, reject) {
-          Client.get("crg/passages").once("value").then(function (data) {
-            resolve({passages: data.val().filter(notNull)});
+          Client.get("crg").once("value").then(function (data) {
+            resolve({passages: data.val().passages.map(withId).filter(notNull)});
           });
         });
 
