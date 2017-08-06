0564f7140b27464ca24842335e83a14e0a7abe9c
nishant <nishant.singh87@gmail.com>
2017-08-06 12:31:17 +0530

    Save end of scene event.

--- src/crg/crg-routes.js
+++ src/crg/crg-routes.js
@@ -1,7 +1,17 @@
-var SceneRoutes = {
+var SceneEvent = require("./scenes/scene-event"),
+    SceneRoutes = {
     load: function(app){
       app.get('/crg/scenes/:id', function(request, response) {
         response.send(JSON.stringify({message: "You asked for " +request.params["id"]}))
+      });
+
+      app.post('/crg/passages/:passageId/scenes/:sceneId/events/end-of-scene', function(request, response) {
+        var passageId = request.param("passageId"),
+            sceneId   = request.param("sceneId"),
+            success   = function (data) {response.send(JSON.stringify(data))},
+            error     = function (data) {response.send(JSON.stringify(data))};
+
+        SceneEvent.endOfScene(passageId, sceneId, request.body.event).then(success, error);
       });
 
     }
---
+++ src/crg/scenes/scene-event.js
@@ -0,0 +1,29 @@
+var Promise = require("promise"),
+    Client = require("../../client"),
+    SceneEvent = {
+      endOfScene: function (passageId, sceneId, event) {
+        return new Promise(function (resolve, reject) {
+          var reference = "crg/passages/:passageId/scenes/:sceneId/"
+                            .replace(":passageId", passageId)
+                            .replace(":sceneId", sceneId),
+              timeSpent = parseInt(event.timeSpent) || 0,
+              incorrectAnswers = parseInt(event.incorrectAnswer) || 0;
+
+          Client.get(reference).once("value").then(function (data) {
+            var scene = data.val() || {
+                  timesPlayed: 0,
+                  sumOfTimeSpent: 0,
+                  incorrectAnswer: 0
+                };
+            scene.timesPlayed     = scene.timesPlayed + 1;
+            scene.sumOfTimeSpent  = scene.sumOfTimeSpent + timeSpent;
+            scene.incorrectAnswer = scene.incorrectAnswer + incorrectAnswers;
+            Client.get(reference).set(scene).then(function(){
+              resolve({scene: scene})});
+            });
+
+        });
+      }
+    }
+
+module.exports = SceneEvent;
